8052c208e7bcfab0c500b77a79f65aa5e0c61968
Zarijaden <Zarijaden@163.com>
2026-02-15 14:34:45 +0800

    手动回溯

--- themes/redefine/source/js/layouts/utteranc.js
+++
@@ -1,8 +0,0 @@
-<script src="https://utteranc.es/client.js"
-            repo="Zarijaden/blog"
-            issue-term="pathname"
-            label="评论"
-            theme="github-light"
-            crossorigin="anonymous"
-            async>
-</script>
